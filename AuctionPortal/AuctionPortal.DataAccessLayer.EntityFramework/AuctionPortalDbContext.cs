f9428fd29451c33780099edd973ade8c014e48f3
Martin Horelka <xhorelka@fi.muni.cz>
2019-10-01 19:36:31 +0200

    Project created

---
+++ AuctionPortal/AuctionPortal.DataAccessLayer.EntityFramework/AuctionPortalDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionPortal.DataAccessLayer.EntityFramework
+{
+    public class AuctionPortalDbContext : DbContext
+    {
+        private const string ConnectionString = "Data source=(localdb)\\mssqllocaldb;Database=AuctionPortalDbContext;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        /// <summary>
+        /// Non-parametric ctor used by data access layer
+        /// </summary>
+        public AuctionPortalDbContext() : base(ConnectionString)
+        {
+            //Database.SetInitializer(new DatabaseInitializer());
+        }
+
+        /// <summary>
+        /// Ctor with db connection, required by data access layer tests
+        /// </summary>
+        /// <param name="connection">The database connection</param>
+        public AuctionPortalDbContext(DbConnection connection) : base(connection, true)
+        {
+            Database.CreateIfNotExists();
+        }
+
+
+        //TODO: sets
+    }
+}
---
+++ AuctionPortal/AuctionPortal.DataAccessLayer.EntityFramework/Entities/IEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionPortal.DataAccessLayer.EntityFramework.Entities
+{
+    public interface IEntity
+    {
+        /// <summary>
+        /// Unique id of the entity.
+        /// </summary>
+        Guid Id { get; set; }
+
+        /// <summary>
+        /// Name of the database table for this entity.
+        /// </summary>
+        string TableName { get; }
+    }
+}
