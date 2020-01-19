f2c1b59612dfbd1ed5a5ab446f769cd8a5058063
Jan Pavlicek <xpavlic@fi.muni.cz>
2020-01-19 23:28:18 +0100

    change email & change password

---
+++ AuctionPortal/AuctionPortal/AuctionPortal.PresentationLayer/Models/Accounts/ChangePasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuctionPortal.PresentationLayer.Models.Accounts
+{
+    public class ChangePasswordModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordAgain { get; set; }
+    }
+}
